8e55e072d31675b9281cebdfc8559724e72a4f4c
Israel Aguilar <iaguilaror@gmail.com>
2023-10-15 00:48:58 -0600

    new ver

---
+++ create-group-or-user/create-group.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Define default values for optional arguments
+zfs_create="no"
+zfs_name=""
+quota=""
+mountpoint=""
+
+# Parse the command-line arguments
+while getopts ":G:g:c:z:q:m:" opt; do
+  case $opt in
+    G)
+      gid="$OPTARG"
+      ;;
+    g)
+      group_name="$OPTARG"
+      ;;
+    c)
+      zfs_create="$OPTARG"
+      ;;
+    z)
+      zfs_name="$OPTARG"
+      ;;
+    q)
+      quota="$OPTARG"
+      ;;
+    m)
+      mountpoint="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Check if the GID is already used by a group
+if getent group "$gid" > /dev/null; then
+  echo "Error: GID $gid is already in use by another group."
+  exit 1
+fi
+
+# Check if the group name is already used by a group
+if getent group "$group_name" > /dev/null; then
+  echo "Error: Group name '$group_name' is already in use by another group."
+  exit 1
+fi
+
+# If zfs-create is "yes," call create-zfs.sh script with -z, -q, and -m arguments
+if [ "$zfs_create" = "yes" ]; then
+  if [ -n "$zfs_name" ] && [ -n "$quota" ] && [ -n "$mountpoint" ]; then
+    ./create-zfs.sh -z "$zfs_name" -q "$quota" -m "$mountpoint"
+    # Change the owner group of the mountpoint to the group name
+    if [ -n "$mountpoint" ]; then
+      chown -R :"$group_name" "$mountpoint"
+    fi
+  else
+    echo "Error: Missing arguments for ZFS creation."
+    exit 1
+  fi
+fi
+
+# Create the group with the given GID and name
+groupadd -g "$gid" "$group_name"
+
+echo "Group creation and ZFS setup (if applicable) completed successfully."
