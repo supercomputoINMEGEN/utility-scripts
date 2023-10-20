457d0de4870e894363c8687f63d6888d44b29116
Lab-Aguilar <iaguilaror@gmail.com>
2023-10-19 23:27:10 -0600

    Update install_miniconda.sh

--- installers/install_miniconda.sh
+++ installers/install_miniconda.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh \
 && bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda -to-bash-profile yes \
 && rm Miniconda3-latest-Linux-x86_64.sh
